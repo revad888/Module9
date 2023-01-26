e92fed93918821cf93d73388ce623250898f4951
Revenko Vladislav <revad8@gmail.com>
2023-01-26 22:20:51 +0300

    done

---
+++ HW/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Exception[] exceptions =
+        {
+            new IndexOutOfRangeException("Exception 1"),
+            new FieldAccessException("Exception 2"),
+            new AccessViolationException("Exception 3"),
+            new FileLoadException("Exception 4"),
+            new MyException("This is my exception")
+
+
+        };
+        foreach (Exception ex in exceptions)
+        {
+            try
+            {
+                throw ex;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        Console.ReadKey();
+    }
+
+    class MyException : Exception
+    {
+        public MyException(string _message) : base(_message) { }
+    }
+}
+
---
+++ HW/Task2/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Task2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var Families = new List<string> { "Иванов", "Богданов", "Сидоров", "Пахомов", "Смычкова" };
+        DataReader dr = new DataReader();
+        DataReader.SortList += Sort;
+        dr.data = Families;
+        try
+        {
+            dr.Read();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.ReadKey();
+    }
+
+    class DataReader
+    {
+
+        public static event Action<List<string>, int> SortList;
+        public List<string> data { get; set; }
+        public void Read()
+        {
+            Console.WriteLine("Введите 1 или 2");
+
+            int num = Int32.Parse(Console.ReadLine());
+
+            if (num != 1 && num != 2) throw new FormatException();
+
+            SortList.Invoke(data, num);
+
+        }
+
+
+    }
+
+    public static void Sort(List<string> list, int num)
+    {
+        switch (num)
+        {
+            case 1:
+                {
+                    list.Sort();
+                    foreach (string i in list)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    break;
+                }
+            case 2:
+                {
+                    list.Sort();
+                    list.Reverse();
+                    foreach (string i in list)
+                    {
+                        Console.WriteLine(i);
+
+                    }
+                    break;
+                }
+        }
+    }
+}
+
+
